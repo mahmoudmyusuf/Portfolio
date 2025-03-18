598818a66ce7843aeeb97a92aeebca84d36090c9
Mahmoud Yusuf <61907105+mahmoudmyusuf@users.noreply.github.com>
2025-03-18 04:41:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,19 +13,6 @@
 
     // Smooth scrolling on the navbar links
     $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
         // ====== HANDLE HASH WHEN NAVIGATING FROM ANOTHER PAGE ======
         if (window.location.hash) {
             setTimeout(function () {
